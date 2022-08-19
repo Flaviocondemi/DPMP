801bc20b84649bfa7043d0a29a29538681f060b3
flavio condemi <flaviovincenzo.condemi@gmail.com>
2022-08-19 11:59:02 +0200

    Initial commit

---
+++ src/main/java/com/flavio/dpmp/DpmpApplication.java
@@ -0,0 +1,13 @@
+package com.flavio.dpmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpmpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flavio/dpmp/DpmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavio.dpmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
